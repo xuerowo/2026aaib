392934c3737010610e90192073e801fe3653af7b
xuerowo <a2006950123@gmail.com>
2026-05-06 11:31:31 +0800

    week11

---
+++ week11/week11-6b.cpp
@@ -0,0 +1,11 @@
+// week11-6b.cpp SOIT108_Advance_004
+#include <stdio.h>
+int main()
+{
+    int n, ans = 0;
+    scanf("%d", &n);
+    for (int i=1; i<=n; i++) {
+        ans += i*11;
+    }
+    printf("%d", ans);
+}
